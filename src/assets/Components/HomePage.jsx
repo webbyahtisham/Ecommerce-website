4d55ad9835b139ebe8f364884ecfec6668b3d6f9
webbyahtisham <ahtishamulhaq384@gmail.com>
2025-02-02 21:02:56 +0500

    createa const function and first section className=hero

---
+++ src/assets/Components/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <>
+            <section className="hero">
+                <div className="hero-container">
+
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default HomePage
