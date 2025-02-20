4e6279f14ddda849ed17747407ae35152eee8eee
webbyahtisham <ahtishamulhaq384@gmail.com>
2025-02-20 15:25:39 +0500

    Add s new file peoduct.js where all the products are store

---
+++ src/assets/Pages/Product.jsx
@@ -0,0 +1,37 @@
+import img1 from '../Images/image 7.png'
+const products = [
+    {
+      id: 1,
+      title: 'Gradient Graphic T-shirt',
+      img: img1,
+      price: 145,
+      rating: 3.5,
+    },
+    {
+      id: 2,
+      title: 'Polo with Tipping Details',
+      img: img1,
+      price: 180,
+      rating: 4.5,
+    },
+    {
+      id: 3,
+      title: 'Black Striped T-shirt',
+      img: img1,
+      price: 150,
+      discountPrice: 120,
+      discount: 30,
+      rating: 5.0,
+    },
+    {
+      id: 4,
+      title: 'Black Striped T-shirt',
+      img: img1,
+      price: 150,
+      discountPrice: 120,
+      discount: 30,
+      rating: 5.0,
+    },
+  ];
+  export default products;
+  
