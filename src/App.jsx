66df584c464498f4365b311c91d17755b6ca57fc
webbyahtisham <ahtishamulhaq384@gmail.com>
2025-02-26 22:10:14 +0500

    Make cart perfect make it responsive more

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Shop from './assets/Pages/Shop';
-import ProductDetail from './assets/Pages/ProductDetail';
+// import { Routes, Route } from 'react-router-dom';
+// import Shop from './assets/Pages/Shop';
+// import ProductDetail from './assets/Pages/ProductDetail';
+import Cart from './assets/Pages/Cart'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Shop />} />
-      <Route path="/product/:id/:title" element={<ProductDetail />} />
-    </Routes>
+    // <Routes>
+    //   <Route path="/" element={<Shop />} />
+    //   <Route path="/product/:id/:title" element={<ProductDetail />} />
+    // </Routes>
+    <Cart/>
   );
 }
 
