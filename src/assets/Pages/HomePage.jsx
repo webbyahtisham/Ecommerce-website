9576ea03bb8f324f3c273b957e8c71363004a1f2
webbyahtisham <ahtishamulhaq384@gmail.com>
2025-06-12 14:33:37 +0500

    update the shop now button add navigations

--- src/assets/Pages/HomePage.jsx
+++ src/assets/Pages/HomePage.jsx
@@ -3,6 +3,7 @@ import NewArrival from '../Components/NewArrival'
 import TopSelling from '../Components/TopSelling'
 import Subscribe from '../Components/Subscribe'
 import Button from '../Components/Button'
+import { NavLink } from 'react-router-dom';
 const HomePage = () => {
     return (
         <>
@@ -12,7 +13,7 @@ const HomePage = () => {
                         <div className="hero-1">
                             <h1 className='hero-1-h1-1'>Find cloths <br /> that matches <br /> your style</h1>
                             <p className='hero-1-p-1'>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
-                            <Button name={"shop now"} />
+                            <Button name="Shop Now" link="/shop" />
                         </div>
                         <div className="hero-2">
                             <img src="/Images/Hero.png" alt="heroimg" />
